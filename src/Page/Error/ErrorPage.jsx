e147c2f05b5ab8bc8481e7cec2ee86510a8a578f
Fahima <sfahima1388@gmail.com>
2023-10-08 01:49:47 +0600

    updated error page

--- src/Page/Error/ErrorPage.jsx
+++ src/Page/Error/ErrorPage.jsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1 className="text-center ">404 Page not found</h1>
-    </div>
+    <>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/tQNz6Qc/circle-scatter-haikei.png)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">404</h1>
+            <p className="mb-5">Sorry the page you requested was not found.</p>
+            <button className="btn btn-accent">
+              <Link to="/">Go to Home</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
