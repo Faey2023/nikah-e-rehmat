8b07b1a36114e8153b51ce883688c2d9199fa6a7
Fahima <sfahima1388@gmail.com>
2023-10-08 01:16:17 +0600

    added easing and duration to aos animation

--- src/Components/gallery/Gallery.jsx
+++ src/Components/gallery/Gallery.jsx
@@ -10,84 +10,128 @@ const Gallery = () => {
         </h1>
 
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          <div data-aos="fade-down">
+          <div data-aos="flip-down">
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/Jct8DXV/11.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-up">
+          <div
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/nj7V1nX/e15646e87b67282c936af6e7e414ddf0.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-down">
+          <div
+            data-aos="flip-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/09JvH5W/b8ace3942d6f23f6a83ad384ce02a0b1.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-up">
+          <div
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/6wn24bt/6840acaad92327ed2e551385e355b97d.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-down">
+          <div
+            data-aos="flip-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/St7bkSj/8-2.webp"
               alt=""
             />
           </div>
-          <div data-aos="fade-up">
+          <div
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/pZSzbmv/12.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-down">
+          <div
+            data-aos="flip-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/F4QnLVX/2.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-up">
+          <div
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/6gTJ1tC/3.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-down">
+          <div
+            data-aos="flip-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/6wBKrZF/5.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-up">
+          <div
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/gSZ9nwN/7.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-down">
+          <div
+            data-aos="flip-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/s9tmDWD/9.jpg"
               alt=""
             />
           </div>
-          <div data-aos="fade-up">
+          <div
+            data-aos="fade-up"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
             <img
               className="h-full w-full rounded-lg"
               src="https://i.ibb.co/54rTYZH/10.jpg"
