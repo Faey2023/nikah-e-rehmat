243c8b320cb84cbb76c14b9a07a598901c63d17b
Fahima <sfahima1388@gmail.com>
2023-10-08 12:24:39 +0600

    some last moment adjustments

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import UseAuth from "../../custom hooks/UseAuth";
 
 const Navbar = () => {
   const { user, userSignOut } = UseAuth();
-  // navlink
+  // navlinks
   const navLinks = (
     <>
       <NavLink
@@ -100,8 +100,7 @@ const Navbar = () => {
                 className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
               >
                 <div className=" font-semibold border">
-                  <li>{user?.displayName}</li>
-                  <li>{user?.email}</li>
+                  <li>User name: {user?.displayName}</li>
                 </div>
                 <li>
                   <Link className="justify-between" to="/profile">
--- src/custom hooks/UseAuth.jsx
+++ src/custom hooks/UseAuth.jsx
@@ -2,8 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 
 const UseAuth = () => {
-  const all = useContext(AuthContext);
-  return all;
+  return useContext(AuthContext);
 };
 
 export default UseAuth;
